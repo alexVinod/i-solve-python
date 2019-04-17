69dc580a33075d878a6dc3dfc90031a6ec8b7516
alexVinod <39164427+alexVinod@users.noreply.github.com>
2019-04-17 12:04:47 +0530

    MaxOf 3

---
+++ 1. Finding maximum of 3 numbers.py
@@ -0,0 +1,9 @@
+a=input("Enter Number1 :")
+b=input("Enter Number2 :")
+c=input("Enter Number3 :")
+
+try:
+    a,b,c = int(a),int(b),int(c)
+    print("Max is :",max(int(a),b,c))
+except Exception as e:
+    print(e,'\n',"Given Only Integer")
